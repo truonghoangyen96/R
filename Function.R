b5dc13423db9a447ee6b3305c2d865aedbde4b65
truonghoangyen96 <70083958+truonghoangyen96@users.noreply.github.com>
2020-08-23 11:27:06 +0700

    Add files via upload

--- Function.R
+++ Function.R
@@ -11,3 +11,30 @@ sum_even.function = function(start, end){
 drink.function = function(price, type='Tea'){
     print(paste('With', price, ', you can drink', type))
 }
+
+# Áp dụng biểu phí 2020
+tinh_tien_dien.function = function(so_kw){
+    muc1 = 1678
+    muc2 = 1734
+    muc3 = 2014
+    muc4 = 2536
+    muc5 = 2834
+    muc6 = 2927
+    bac50 = 50
+    bac100 = 100
+    tien_dien = 0
+    if (so_kw <= 50){
+        tien_dien = so_kw * muc1
+    } else if (so_kw <=100){
+        tien_dien = bac50*muc1 + (so_kw-bac50)*muc2
+    } else if (so_kw <=200){
+        tien_dien = bac50*muc1 + bac50*muc2 + (so_kw-bac100)*muc3
+    } else if (so_kw <=300){
+        tien_dien = bac50*muc1 + bac50*muc2 + bac100*muc3 + (so_kw-bac50-bac50-bac100)*muc4
+    } else if (so_kw <=400){
+        tien_dien = bac50*muc1 + bac50*muc2 + bac100*muc3 + bac100*muc4 + (so_kw-bac50-bac50-bac100-bac100)*muc5
+    } else{
+        tien_dien = bac50*muc1 + bac50*muc2 + bac100*muc3 + bac100*muc4 + bac100*muc5 + (so_kw-bac50-bac50-bac100-bac100-bac100)*muc6
+    }
+    print(paste('Tien dien phai tra =', paste(format(tien_dien, big.mark = '.', decimal.mark = ','), sep='')))
+}
